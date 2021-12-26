02564b6be184bacdc8721c0e353e7b9e6d7fb9f7
zzoeys <zzoeysoh@gmail.com>
2021-12-26 22:52:56 +0000

    Save HTML file with choropleth map

---
+++ choropleth_map.py
@@ -0,0 +1,10 @@
+import geopandas as gpd
+import pandas as pd
+import plotly.express as px
+import json
+import numpy as np
+from urllib.request import urlopen
+import plotly.graph_objs as go
+import pyproj
+
+
